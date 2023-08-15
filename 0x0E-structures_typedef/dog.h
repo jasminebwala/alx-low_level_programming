2cfd28148995ad86585d8e18569034be018dbef0
jasminiebwala <jasminebwala32@gmail.com>
2023-08-15 14:40:23 -0700

    bob

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,19 +1,23 @@
-#include <stdio.h>
-#include "dog.h"
+#ifndef DOG_H
+#define DOG_H
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * struct dog - Define a new type struct dog
+ * @name: Dog name.
+ * @age: Dog age.
+ * @owner: Dog owner.
  */
-int main(void)
+struct dog
 {
-    dog_t my_dog;
+	char *name;
+	float age;
+	char *owner;
+};
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
 
-    my_dog.name = "Poppy";
-    my_dog.age = 3.5;
-    my_dog.owner = "Bob";
-    printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
-    return (0);
-}
 
